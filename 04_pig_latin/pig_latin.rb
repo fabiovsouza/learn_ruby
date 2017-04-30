f6236322aaa3dad8ba871fe16a40014e13d5a769
Fabio Souza <fabio.virginio.souza@gmail.com>
2017-04-29 21:11:15 -0300

    4th exercise finished.

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,50 @@
-#write your code here
+
+def translate string
+
+  vowels =  ['a','e','i','o','A','E','I','O']
+
+  arrayString = string.split(' ')
+
+    for i in 0..(arrayString.length - 1)
+
+      arrayWord = arrayString[i].split(//)
+
+      vowelFound = false
+
+      for j in 0..(arrayWord.length-1)
+
+          while not (vowelFound)
+
+            if not (vowels.include?(arrayWord[j]))
+
+              if (j == 0) and (arrayWord[j] == arrayWord[j].upcase)
+
+                  arrayWord[j].downcase!
+                  arrayWord.push(arrayWord.shift)
+                  arrayWord[j].upcase!
+
+              else
+
+                arrayWord.push(arrayWord.shift)
+
+              end
+
+            else
+
+              vowelFound = true
+
+            end
+
+          end
+
+      end
+
+        arrayString[i] = arrayWord.join + 'ay'
+
+    end
+
+    return arrayString.join(' ')
+
+end
+
+puts translate("The Quick Brown Fox")
--- 04_pig_latin/pig_latin_spec.rb
+++ 04_pig_latin/pig_latin_spec.rb
@@ -65,6 +65,11 @@ describe "#translate" do
     expect(s).to eq("ethay ickquay ownbray oxfay")
   end
 
+  it "keeps capitalized words" do
+    s = translate("The Quick Brown Fox")
+    expect(s).to eq("Ethay Ickquay Ownbray Oxfay")
+  end
+
   # Test-driving bonus:
   # * write a test asserting that capitalized words are still capitalized (but with a different initial capital letter, of course)
   # * retain the punctuation from the original phrase
