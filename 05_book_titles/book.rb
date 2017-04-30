641a8398061c71db731428de5fb64d5a961a53b1
Fabio Souza <fabio.virginio.souza@gmail.com>
2017-04-30 11:29:18 -0300

    Finished all 6 exercises

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,29 @@
 class Book
-# write your code here
+
+  attr_accessor :title
+
+  def title
+
+    exceptions = ['the','a','an','and','in','of']
+
+    arrayString = @title.split(' ')
+
+    for i in (0..arrayString.length - 1)
+
+      if i == 0
+
+        arrayString[i].capitalize!
+
+      elsif not (exceptions.include?(arrayString[i]))
+
+        arrayString[i].capitalize!
+
+      end
+
+    end
+
+    return arrayString.join(' ')
+
+  end
+
 end
--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,17 @@
 class Timer
-  #write your code here
+
+  attr_accessor :seconds
+
+  def initialize
+    @seconds = 0
+  end
+
+  def time_string
+
+    time = Time.mktime(2000,10,10) + @seconds.to_i
+
+    return time.strftime("%H:%M:%S")
+
+  end
+
 end
